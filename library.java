eb1084d800090834f7a3eac4dc1f74a344a8e466
Raj Prasad <rajeshwarrprasad@yahoo.com>
2024-04-24 12:12:20 +1200

    Initialise program

---
+++ library.java
@@ -0,0 +1,13 @@
+
+/**
+ * basic library system in Java. The program should allow users to add books to the library, 
+ * borrow books, and return books. The system maintains a record of the available quantity 
+ * of each book.
+ */
+public class library {
+    public static void main(String[] args) {
+        System.out.println("Welcome to basic library");
+    }
+    
+}
+
