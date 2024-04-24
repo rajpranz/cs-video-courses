bc2f1312ef0b7246cb3db2d549643506836e2bd1
Raj Prasad <rajeshwarrprasad@yahoo.com>
2024-04-24 12:34:01 +1200

    Add main menu

--- library.java
+++ library.java
@@ -4,10 +4,21 @@
  * borrow books, and return books. The system maintains a record of the available quantity 
  * of each book.
  */
-public class library {
-    public static void main(String[] args) {
-        System.out.println("Welcome to basic library");
-    }
-    
-}
 
+ import textio.TextIO;
+
+public class Library {
+    public static void main(String[] args) {
+        Book[] books;
+        int selection = 0;
+        while (selection!=4) {
+            System.out.println("Welcome to basic library");
+            System.out.println("1. Add book");
+            System.out.println("2. Borrow book");
+            System.out.println("3. Return book");
+            System.out.println("4. Exit");
+            selection = TextIO.getlnInt();    
+        }
+        
+    }    
+} 
